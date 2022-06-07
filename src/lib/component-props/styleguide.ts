3d79c79b2f2338f6d397e36fa12ecdd187d8de9e
Federico Mujica <federicomujica1@gmail.com>
2022-06-07 19:12:08 +0200

    Initial Commit

---
+++ src/lib/component-props/styleguide.ts
@@ -0,0 +1,10 @@
+import { Field } from '@sitecore-jss/sitecore-jss-nextjs';
+/**
+ * Shared styleguide specimen fields
+ */
+export type StyleguideSpecimenFields = {
+  fields: {
+    description: Field<string>;
+    heading: Field<string>;
+  };
+};
